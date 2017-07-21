c8cd1947a1a29632b3083c34b7d75d1a6345c530
Connor Edwards <unst4bl3.alpha@gmail.com>
2017-07-21 23:33:24 +0000

    General ES6 refractor.

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-var config = {};
-
-config.deviceid = false; // When set to false, an anonymous device id will be asigned.
-config.interval = 1000; // The amount of delay between each request in ms (milliseconds)
-config.enableHeartbeat = true; // Please enable heartbeat to share and receive anonymous statistics about the amount of requests being made
-config.autoUpdateData = true; // Please enable to make sure you always get the latest dataset
-
-module.exports = config;
+module.exports = {
+	"deviceID": false,				// When set to false, an anonymous device id will be asigned.			
+	"interval": 1000,					// The amount of delay between each request in ms (milliseconds).			
+	"enableHeartbeat": true, 	// Please enable heartbeat to share and receive anonymous statistics about the amount of requests being made.
+	"autoUpdateData": true		// Please enable to make sure you always get the latest dataset.
+};
