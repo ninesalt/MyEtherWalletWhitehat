205bf60962725f217f53129ba3756232aaf68abe
MrLuit <me@luithollander.nl>
2017-07-21 11:26:09 +0200

    Updated dataset and raised interval

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.interval = 100; // The amount of delay between each request in ms (milliseconds)
+config.interval = 1000; // The amount of delay between each request in ms (milliseconds)
 config.enableHeartbeat = true; // Please enable heartbeat to share and receive anonymous statistics about the amount of requests being made
 
 module.exports = config;
